535dac5564bb0989cd552bc278c7a16a568bd43c
mlipov <maxim.lipov@gmail.com>
2023-12-15 08:49:46 +0200

    231212 CW-53 Initial code of Cars Web Service with Controller test (method addCar)

---
+++ src/main/java/telran/cars/OwnersCarsApplication.java
@@ -0,0 +1,12 @@
+package telran.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnersCarsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OwnersCarsApplication.class, args);
+	}
+
+}
